9c131b55e24ed645a3debaa22041dc6e6ff5dc6e
duduu162-create <duduu162@gmail.com>
2026-09-17 14:06:42 -0300

    add lead status model

---
+++ src/main/java/com/fisioroyale/leads/model/LeadStatus.java
@@ -0,0 +1,15 @@
+package com.fisioroyale.leads.model;
+
+public enum LeadStatus {
+    NOVO,
+    TENTATIVA_CONTATO,
+    CONTATO_REALIZADO,
+    INTERESSADO,
+    AGENDADO,
+    COMPARECEU,
+    NEGOCIACAO,
+    CONVERTIDO,
+    SEM_RESPOSTA,
+    NAO_COMPARECEU,
+    PERDIDO
+}
